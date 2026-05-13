b8c0eeca1b7d170aa30177677ee3f864a99e7e20
Lesly <lesly15chamba@gmail.com>
2026-05-13 11:20:03 -0500

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -31,11 +31,11 @@ class Settings(BaseSettings):
     POSTGRES_DB: str = ""
 
     # Modelos ML
-    MASS_MODEL_PATH: str = "C:/Users/HP/Documents/tesis_ganado_jersey/Jersey-Prediccion/models_pt/02_Models/Peso/best.pt"
-    BCS_MODEL_PATH: str = "C:/Users/HP/Documents/tesis_ganado_jersey/Jersey-Prediccion/models_pt/02_Models/BCS/best.pt"
     SAM_CHECKPOINT_PATH: str = "models_pt/sam_checkpoint.pth"
     YOLO_BCS_PATH: str = "models_pt/yolov8_bcs.pt"
-
+    MASS_MODEL_PATH: str = "app/models_pt/Peso/best.pt"
+    BCS_MODEL_PATH: str = "app/models_pt/BCS/best.pt"
+    
     # Hugging Face
     HF_TOKEN: str = ""       
     HF_REPO_ID: str = ""     
