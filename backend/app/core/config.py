191e530089d118e4343f6bf5b088f72465d3f50e
Lesly <lesly15chamba@gmail.com>
2026-05-14 10:55:36 -0500

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -34,8 +34,8 @@ class Settings(BaseSettings):
     # Modelos ML
     SAM_CHECKPOINT_PATH: str = "models_pt/sam_checkpoint.pth"
     YOLO_BCS_PATH: str = "models_pt/yolov8_bcs.pt"
-    MASS_MODEL_PATH: str = "app/models_pt/Peso/best.pt"
-    BCS_MODEL_PATH: str = "app/models_pt/BCS/best_BCS.pt"
+    MASS_MODEL_PATH: str = "models_pt/Peso/best.pt"
+    BCS_MODEL_PATH: str = "models_pt/BCS/best_BCS.pt"
     
     # Hugging Face
     HF_TOKEN: str = ""       
