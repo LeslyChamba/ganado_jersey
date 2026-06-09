c19d7e304be92d98d7043ad7e1c9966242a13b6f
LeslyChamba <136204975+LeslyChamba@users.noreply.github.com>
2026-06-09 04:38:45 +0000

    cambio ruta

--- frontend/src/components/RecuperarPassword.jsx
+++ frontend/src/components/RecuperarPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import api from "../../services/api"
+import api from "../services/api"
 
 export default function RecuperarPassword() {
   const [email, setEmail]     = useState("")
